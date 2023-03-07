e0309da2d4de8f0dfdaf9c68a379cf283e1bc478
Lidor <lidororanim@gmail.com>
2023-03-07 11:08:30 +0200

    added worker.yaml and script to customize the yaml files

---
+++ scripts/k8s_deployment_yaml_customize.py
@@ -0,0 +1,17 @@
+import yaml
+import os
+
+
+env = os.getenv("APP_ENV")
+image = os.getenv("BOT_IMAGE_NAME")
+file_to_save = f'infra/k8s/{os.getenv("K8S_DEPLOYMENT_FILE")}'
+
+with open(f"../infra/k8s/{file_to_save}") as file:
+    y = yaml.safe_load(file)
+    y["metadata"]["labels"]["env"] = env
+    y["spec"]["selector"]["matchLabels"]["env"] = env
+    y["spec"]["template"]["metadata"]["labels"]["env"] = env
+    y["spec"]["template"]["spec"]["containers"][0]["image"] = image
+
+with open(file_to_save, 'w') as deploy:
+    yaml.dump(y, deploy)
