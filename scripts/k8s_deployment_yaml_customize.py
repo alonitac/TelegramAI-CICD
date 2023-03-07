e2b088a163f96f8cda57360e55ec0814a178cb3a
Lidor <lidororanim@gmail.com>
2023-03-07 13:17:14 +0200

    fixed script path

--- scripts/k8s_deployment_yaml_customize.py
+++ scripts/k8s_deployment_yaml_customize.py
@@ -6,7 +6,7 @@ env = os.getenv("APP_ENV")
 image = os.getenv("BOT_IMAGE_NAME")
 file_to_save = f'infra/k8s/{os.getenv("K8S_DEPLOYMENT_FILE")}'
 
-with open(f"..{file_to_save}") as file:
+with open(f"../{file_to_save}") as file:
     y = yaml.safe_load(file)
     y["metadata"]["labels"]["env"] = env
     y["spec"]["selector"]["matchLabels"]["env"] = env
