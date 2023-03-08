ee6175a1c072a6896be9ee1bf8cd048da03d7e85
Lidor <lidororanim@gmail.com>
2023-03-08 10:58:41 +0200

    updated script to support worker

--- scripts/k8s_deployment_yaml_customize.py
+++ scripts/k8s_deployment_yaml_customize.py
@@ -3,7 +3,8 @@ import os
 
 
 env = os.getenv("APP_ENV")
-image = os.getenv("BOT_IMAGE_NAME") if True else os.getenv("WORKER_IMAGE_NAME")
+bot_image = os.getenv("BOT_IMAGE_NAME")
+worker_image = os.getenv("WORKER_IMAGE_NAME")
 file_to_save = f'{os.getenv("K8S_DEPLOYMENT_FILE")}'
 file_to_open = f'{os.getenv("K8S_YAML_TO_EDIT")}'
 
@@ -12,8 +13,14 @@ with open(file_to_open) as file:
     y["metadata"]["labels"]["env"] = env
     y["spec"]["selector"]["matchLabels"]["env"] = env
     y["spec"]["template"]["metadata"]["labels"]["env"] = env
-    y["spec"]["template"]["spec"]["containers"][0]["image"] = image
     y["spec"]["template"]["spec"]["containers"][0]["env"][0]["value"] = env
+
+    if bot_image:
+        y["spec"]["template"]["spec"]["containers"][0]["image"] = bot_image
+    else:
+        y["spec"]["template"]["spec"]["containers"][0]["image"] = worker_image
+
+
 
 with open(file_to_save, 'w') as deploy:
     yaml.dump(y, deploy)
