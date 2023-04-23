e529cbff2cdea432f184712a518263c58ac769cb
Tamir Nator <Tamir.Nator@philips.com>
2023-04-23 14:15:13 +0300

    fix

--- deploy/terragrunt/tfscripts/k0s-init.sh
+++ deploy/terragrunt/tfscripts/k0s-init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 (
-su ec2-user -c 'curl -o /home/ec2-user/ecr-creds-helper.yaml https://raw.githubusercontent.com/alonitac/DevOpsMay22/main/cicd_ex_helpers/ecr-creds-helper.yaml'
+sudo su ec2-user -c 'curl -o /home/ec2-user/ecr-creds-helper.yaml https://raw.githubusercontent.com/alonitac/DevOpsMay22/main/cicd_ex_helpers/ecr-creds-helper.yaml'
 chmod 777 /home/ec2-user/ecr-creds-helper.yaml
-su ec2-user -c 'curl https://raw.githubusercontent.com/TamirNator/TelegramAI-CICD/main/deploy/terragrunt/tfscripts/init.sh -o /home/ec2-user/init.sh'
+sudo su ec2-user -c 'curl https://raw.githubusercontent.com/TamirNator/TelegramAI-CICD/main/deploy/terragrunt/tfscripts/init.sh -o /home/ec2-user/init.sh'
 chmod 777 /home/ec2-user/init.sh
 sudo su - ec2-user /home/ec2-user/init.sh
-su ec2-user -c 'kubctl create namespace dev'
-su ec2-user -c 'kubctl create namespace prod'
+cd /home/ec2-user/
+kubectl create namespace dev
+kubectl create namespace prod
 
 ) 2>&1 | sudo tee -a /home/ec2-user/full_log.log
