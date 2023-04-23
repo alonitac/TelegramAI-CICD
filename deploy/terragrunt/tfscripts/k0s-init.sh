aaecbd73aaf08321aa6965742db1bae797f6647d
Tamir Nator <Tamir.Nator@philips.com>
2023-04-23 12:45:17 +0300

    added init.sh script

--- deploy/terragrunt/tfscripts/k0s-init.sh
+++ deploy/terragrunt/tfscripts/k0s-init.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 (
-sudo su ec2-user
-sudo curl -o /home/ec2-user/ecr-creds-helper.yaml https://raw.githubusercontent.com/alonitac/DevOpsMay22/main/cicd_ex_helpers/ecr-creds-helper.yaml
-sudo chmod 777 /home/ec2-user/ecr-creds-helper.yaml
-sudo curl https://raw.githubusercontent.com/alonitac/DevOpsMay22/main/cicd_ex_helpers/init.sh -o /home/ec2-user/init.sh 
-cd /home/ec2-user/
-su ec2-user -c 'bash -e init.sh'
-sudo bash -e init.sh -u ec2-user
+su ec2-user -c 'curl -o /home/ec2-user/ecr-creds-helper.yaml https://raw.githubusercontent.com/alonitac/DevOpsMay22/main/cicd_ex_helpers/ecr-creds-helper.yaml'
+chmod 777 /home/ec2-user/ecr-creds-helper.yaml
+su ec2-user -c 'curl https://raw.githubusercontent.com/TamirNator/TelegramAI-CICD/main/deploy/terragrunt/tfscripts/init.sh -o /home/ec2-user/init.sh'
+chmod 777 /home/ec2-user/init.sh
+su ec2-user -c 'bash -e /home/ec2-user/init.sh'
+su ec2-user -c 'kubctl create namespace dev'
+su ec2-user -c 'kubctl create namespace prod'
+
 ) 2>&1 | sudo tee -a /home/ec2-user/full_log.log
