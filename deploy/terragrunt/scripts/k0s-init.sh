dfcc991351f7e6af625b02606fd80431475ee4a9
Tamir Nator <Tamir.Nator@philips.com>
2023-04-28 16:35:14 +0300

    fix

--- deploy/terragrunt/scripts/k0s-init.sh
+++ deploy/terragrunt/scripts/k0s-init.sh
@@ -2,7 +2,7 @@
 (
 sudo su ec2-user -c 'curl -o /home/ec2-user/ecr-creds-helper.yaml https://raw.githubusercontent.com/alonitac/DevOpsMay22/main/cicd_ex_helpers/ecr-creds-helper.yaml'
 chmod 777 /home/ec2-user/ecr-creds-helper.yaml
-sudo su ec2-user -c 'curl https://raw.githubusercontent.com/TamirNator/TelegramAI-CICD/main/deploy/terragrunt/tfscripts/init.sh -o /home/ec2-user/init.sh'
+sudo su ec2-user -c 'curl https://raw.githubusercontent.com/TamirNator/TelegramAI-CICD/main/deploy/terragrunt/scripts/init.sh -o /home/ec2-user/init.sh'
 chmod 777 /home/ec2-user/init.sh
 sudo su - ec2-user /home/ec2-user/init.sh
 cd /home/ec2-user/
