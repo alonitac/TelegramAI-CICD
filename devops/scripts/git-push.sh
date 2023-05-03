d32be8fdd970bb7c72a89321fff85d69319d6ccb
Tamir Nator <Tamir.Nator@philips.com>
2023-05-03 21:18:36 +0300

    fix

--- devops/scripts/git-push.sh
+++ devops/scripts/git-push.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 git config --global user.name "Jenkis"
 git config --global user.email "Jenkins@example.com"
-git config --global --add safe.directory $4
-git config remote.origin.url $5
 git stash push
 git checkout $2
 git pull origin $2 
