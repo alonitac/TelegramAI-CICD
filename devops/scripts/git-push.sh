d2bc4ca220ad2beb6ecaf7b45f5971141b3f0222
Tamir Nator <Tamir.Nator@philips.com>
2023-05-07 11:23:37 +0300

    fix

--- devops/scripts/git-push.sh
+++ devops/scripts/git-push.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+set -e
 git config --global user.name "Jenkis"
 git config --global user.email "Jenkins@example.com"
 echo "Running git stash push"
 git stash push
 echo "Running git checkout $2"
 git checkout $2
+echo "Running git reset --hard origin/$2"
+git reset --hard origin/$2
 echo "Running git pull $2"
-git pull origin $2 
+git pull
 echo "Running git stash apply"
 git stash apply
 echo "git add $1"
