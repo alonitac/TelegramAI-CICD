249551400ee87503cacc9ca3ed9460e0522b9baa
Tamir Nator <Tamirna811@gmail.com>
2023-05-31 22:57:38 +0300

    fix

--- devops/scripts/git-push.sh
+++ devops/scripts/git-push.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -e
-git config --global user.name "Jenkis"
-git config --global user.email "Jenkins@example.com"
+git config --global user.name "github"
+git config --global user.email "github@example.com"
 git config --global pull.rebase false
+
+echo "Running git stash push"
+git stash push
+echo "Running git checkout $2"
+git checkout $2
+echo "Running git reset --hard origin/$2"
+git reset --hard origin/$2
 
 while true
 do
     push_success=1
-    echo "Running git stash push"
-    git stash push
-    echo "Running git checkout $2"
-    git checkout $2
-    echo "Running git reset --hard origin/$2"
-    git reset --hard origin/$2
     echo "Running git pull $2"
     git pull
     echo "Running git stash apply"
